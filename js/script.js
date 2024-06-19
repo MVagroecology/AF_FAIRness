bf893ce6e61a720f651cdb1abd5d1965002faaaf
anatomas <anatomas@mvarc.eu>
2024-06-19 11:21:35 +0100

    making router work

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ Object.filter = function(obj, predicate) {
 
 const router = new VueRouter({
 	mode: 'history',
-	base: window.location.pathname,
+	base: window.location.pathname.substring(0, window.location.pathname.indexOf("AF_FAIRness")) + "AF_FAIRness/",
 	routes: [
 		{
 			path: '/',
